033237d69462e75688c3058b60e2b1d52d847b06
ambashar10-star <ambashar10@gmail.com>
2025-09-20 23:05:59 +0300

    Initial commit

---
+++ src/App.java
@@ -0,0 +1,34 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        // Code is written here
+        System.out.println("Printer App");
+        String name;
+        int age = 30;
+        name = "Bashar";
+        //System.out.println(age);
+        // System.out.print("My name is ");
+        // System.out.println(name);
+    //  int number1 = 0;
+    //  int number2 = 0;
+    //  int sum = 0;
+    //  number1 = 10;
+    //  number2 = 5;
+    //  number1 = 100;
+    // //  sum = 50 * 50 + 50;
+    // sum = number1 + number2;
+    //     System.out.println("The sum of the calculation is " + sum);
+
+    int points = 0;
+    // points = points + 1;
+// points++;
+    // System.out.println(points);
+double number1, number2, results;
+number1=10;
+number2= 3;
+results = number1/number2;
+// System.out.println(results);
+System.out.printf("The result is %.2f\n\n", results);
+System.out.printf("\tMy name is %s and I am  %d years old.", name, age);
+
+    }
+}
