96f16162182489b78329d3f96d345176de9668f5
ambashar10-star <ambashar10@gmail.com>
2025-09-20 23:47:06 +0300

    changed the format variables
    Changed the format of some variables and changed some code to comments

--- src/App.java
+++ src/App.java
@@ -8,27 +8,25 @@ public class App {
         //System.out.println(age);
         // System.out.print("My name is ");
         // System.out.println(name);
-    //  int number1 = 0;
-    //  int number2 = 0;
-    //  int sum = 0;
-    //  number1 = 10;
-    //  number2 = 5;
-    //  number1 = 100;
-    // //  sum = 50 * 50 + 50;
-    // sum = number1 + number2;
-    //     System.out.println("The sum of the calculation is " + sum);
+     int number1 = 10;
+     int number2 = 100;
+     int sum = 0;
+     
+    //  sum = 50 * 50 + 50;
+    sum = number1 + number2;
+        System.out.println("The sum of the calculation is " + sum);
 
     int points = 0;
     // points = points + 1;
 // points++;
     // System.out.println(points);
-double number1, number2, results;
-number1=10;
-number2= 3;
-results = number1/number2;
-// System.out.println(results);
-System.out.printf("The result is %.2f\n\n", results);
-System.out.printf("\tMy name is %s and I am  %d years old.", name, age);
+// double number1, number2, results;
+// number1=10;
+// number2= 3;
+// results = number1/number2;
+// // System.out.println(results);
+// System.out.printf("The result is %.2f\n\n", results);
+// System.out.printf("\tMy name is %s and I am  %d years old.", name, age);
 
     }
 }
